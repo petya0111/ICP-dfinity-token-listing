8131d2915ed9ecd19827edb07f7b3bf26cbc8d21
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-10-04 18:16:41 +0300

    Update NFT_token_index.ts

--- src/NFT_token_index.ts
+++ src/NFT_token_index.ts
@@ -1,9 +1,9 @@
-// cannister code goes here
+// Canister code goes here
 import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
- * This type provides details of listed NFT token on a board.
+ * This type provides details of a listed NFT token on a board.
  */
 
 type ListedToken = Record<{
@@ -33,30 +33,36 @@ export function getListedTokens(): Result<Vec<ListedToken>, string> {
 
 $query;
 export function getListedToken(id: string): Result<ListedToken, string> {
-    return match(tokenStorage.get(id), {
-        Some: (listedToken) => Result.Ok<ListedToken, string>(listedToken),
-        None: () => Result.Err<ListedToken, string>(`a listedToken with id=${id} not found`)
-    });
+    const listedToken = tokenStorage.get(id);
+    if (listedToken === null) {
+        return Result.Err<ListedToken, string>(`A listedToken with id=${id} not found`);
+    }
+    return Result.Ok(listedToken);
 }
 
 $query;
 export function getStatusListedTokenCurrentlyListed(id: string): Result<boolean, string> {
-    return match(tokenStorage.get(id), {
-        Some: (listedToken) => Result.Ok<boolean, string>(listedToken.currentlyListed),
-        None: () => Result.Err<boolean, string>(`a listedToken with id=${id} not found`)
-    });
+    const listedToken = tokenStorage.get(id);
+    if (listedToken === null) {
+        return Result.Err<boolean, string>(`A listedToken with id=${id} not found`);
+    }
+    return Result.Ok(listedToken.currentlyListed);
 }
 
 $query;
 export function getNFTPinataIPFSurl(id: string): Result<string, string> {
-    return match(tokenStorage.get(id), {
-        Some: (listedToken) => Result.Ok<string, string>(listedToken.pinataURL),
-        None: () => Result.Err<string, string>(`a listedToken with id=${id} not found`)
-    });
+    const listedToken = tokenStorage.get(id);
+    if (listedToken === null) {
+        return Result.Err<string, string>(`A listedToken with id=${id} not found`);
+    }
+    return Result.Ok(listedToken.pinataURL);
 }
 
 $update;
 export function addListedToken(payload: ListedTokenPayload): Result<ListedToken, string> {
+    if (!payload.tokenId || !payload.tokenName || !payload.body || !payload.pinataURL) {
+        return Result.Err<ListedToken, string>("Missing required fields in payload");
+    }
     const token: ListedToken = { id: uuidv4(), currentlyListed: true, createdAt: ic.time(), updatedAt: Opt.None, ...payload };
     tokenStorage.insert(token.id, token);
     return Result.Ok(token);
@@ -64,45 +70,49 @@ export function addListedToken(payload: ListedTokenPayload): Result<ListedToken,
 
 $update;
 export function updateListedToken(id: string, payload: ListedTokenPayload): Result<ListedToken, string> {
-    return match(tokenStorage.get(id), {
-        Some: (listedToken) => {
-            const updatedListedToken: ListedToken = {...listedToken, ...payload, updatedAt: Opt.Some(ic.time())};
-            tokenStorage.insert(listedToken.id, updatedListedToken);
-            return Result.Ok<ListedToken, string>(updatedListedToken);
-        },
-        None: () => Result.Err<ListedToken, string>(`couldn't update a listedToken with id=${id}. listedToken not found`)
-    });
+    const listedToken = tokenStorage.get(id);
+    if (listedToken === null) {
+        return Result.Err<ListedToken, string>(`A listedToken with id=${id} not found`);
+    }
+    if (!payload.tokenId || !payload.tokenName || !payload.body || !payload.pinataURL) {
+        return Result.Err<ListedToken, string>("Missing required fields in payload");
+    }
+    const updatedListedToken: ListedToken = { ...listedToken, ...payload, updatedAt: Opt.Some(ic.time()) };
+    tokenStorage.insert(listedToken.id, updatedListedToken);
+    return Result.Ok(updatedListedToken);
 }
+
 $update;
 export function unlistToken(id: string): Result<ListedToken, string> {
-    return match(tokenStorage.get(id), {
-        Some: (listedToken) => {
-            const updatedListedToken: ListedToken = {...listedToken, currentlyListed: false, updatedAt: Opt.Some(ic.time())};
-            tokenStorage.insert(listedToken.id, updatedListedToken);
-            return Result.Ok<ListedToken, string>(updatedListedToken);
-        },
-        None: () => Result.Err<ListedToken, string>(`couldn't update a listedToken with id=${id}. listedToken not found`)
-    });
+    const listedToken = tokenStorage.get(id);
+    if (listedToken === null) {
+        return Result.Err<ListedToken, string>(`A listedToken with id=${id} not found`);
+    }
+    listedToken.currentlyListed = false;
+    listedToken.updatedAt = Opt.Some(ic.time());
+    tokenStorage.insert(listedToken.id, listedToken);
+    return Result.Ok(listedToken);
 }
 
 $update;
 export function deleteListedToken(id: string): Result<ListedToken, string> {
-    return match(tokenStorage.remove(id), {
-        Some: (deletedListedToken) => Result.Ok<ListedToken, string>(deletedListedToken),
-        None: () => Result.Err<ListedToken, string>(`couldn't delete a listedToken with id=${id}. listedToken not found.`)
-    });
+    const deletedListedToken = tokenStorage.remove(id);
+    if (deletedListedToken === null) {
+        return Result.Err<ListedToken, string>(`A listedToken with id=${id} not found`);
+    }
+    return Result.Ok(deletedListedToken);
 }
 
-// a workaround to make uuid package work with Azle
+// A workaround to make the uuid package work with Azle
 globalThis.crypto = {
     // @ts-ignore
-   getRandomValues: () => {
-       let array = new Uint8Array(32)
+    getRandomValues: () => {
+        let array = new Uint8Array(32)
 
-       for (let i = 0; i < array.length; i++) {
-           array[i] = Math.floor(Math.random() * 256)
-       }
+        for (let i = 0; i < array.length; i++) {
+            array[i] = Math.floor(Math.random() * 256)
+        }
 
-       return array
-   }
+        return array
+    }
 }
